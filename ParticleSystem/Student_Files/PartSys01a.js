af4e34f47f4a5846b8892ad6decfb2ff37b1f625
AlexMartin4 <alexismartineau0@gmail.com>
2019-07-07 18:46:46 +0100

    Changed test in Particle System

--- ParticleSystem/Student_Files/PartSys01a.js
+++ ParticleSystem/Student_Files/PartSys01a.js
@@ -954,4 +954,17 @@ PartSys.prototype.swap = function(src, dest) {
 //  this.s1 = tmp;
 }
 
+function newGrid(){
+	var temp =  new GridBox(50, 50, 20);
+	console.log(temp);
+	//temp.init();
+	return temp;
+}
+
+function Float32Edit(base,edit,startIdx){
+  for(var i = 0; i < edit.length;i++){
+    base[i+startIdx] = edit[i];
+  }
+  return base;
+}
 
